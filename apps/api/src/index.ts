017e51880b87f305b59bd16ea0763f815478280c
Mohfazam <mohd.sarwar.code@gmail.com>
2026-03-28 22:11:17 +0530

    Loger

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,10 @@
 import app from "./app";
+import { initSocket, initLogger } from "@repo/realtime";
 
 const PORT = process.env.PORT || 4000;
+
+initSocket(4001);
+initLogger();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
